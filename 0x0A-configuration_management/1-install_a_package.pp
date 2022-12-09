eb70f4d98d568f13edc858363c8eb2e6249983f6
Oluwamuyiwa Lawal <oluwamuyiwasodiq@gmail.com>
2022-12-09 03:27:44 -0800

    Added 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet, install flask from pip3
+# Using Puppet, install flask from pip3 (puppet-lint)
 
 package { 'puppet-lint':
   ensure   => '2.1.0',
-  provider => 'flask',
+  provider => 'gem',
