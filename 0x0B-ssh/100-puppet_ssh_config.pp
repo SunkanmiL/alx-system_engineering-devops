ae6ba736db246b238a9e79dc6dc601e27ee0435e
Oluwamuyiwa <oluwamuyiwasodiq@gmail.com>
2022-12-11 20:39:22 -0800

    Updated 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,9 @@
 # Updates config file using puppet
-include stdlib
-
-file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no',
-  replace => true
+file_line { 'disable password login':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
-
-file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  replace => true
+file_line { 'add path to find the keys':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
 }
