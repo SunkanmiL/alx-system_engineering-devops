a415108ac0b5d7f268f9079c8b30035bd4e89b6b
Oluwamuyiwa Lawal <oluwamuyiwasodiq@gmail.com>
2023-01-17 02:38:18 -0800

    Added 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Using this REST API, for a given employee ID, returns information about
+   his/her TODO list progress"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+    user = requests.get('{}/users/{}'.format(BASE_URL, argv[1])).json()
+    todos = requests.get(
+        '{}/todos'.format(BASE_URL),
+        params={
+            "userId": argv[1]}).json()
+
+    total_tasks = len(todos)
+    completed = 0
+    completed_tasks = []
+    for todo in todos:
+        if todo["completed"]:
+            completed += 1
+            completed_tasks.append(todo.get("title"))
+
+    output = 'Employee {} is done with tasks({}/{}):'.format(
+        user.get("name"),
+        completed,
+        total_tasks)
+    print(output)
+    [print('\t {}'.format(task)) for task in completed_tasks]
