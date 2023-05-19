3cb159d366966c463d7d4d64521c069fb62dd032
Oluwamuyiwa Lawal <oluwamuyiwasodiq@gmail.com>
2023-05-18 23:51:04 -0700

    Added 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,9 @@
 # Puppet script will replace a line in a file on a server
 
-$file_to_edit = '/etc/default/nginx'
-
 # Increase the ULIMIT of the default file
 
 exec { 'replace_line':
-  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 4096\"/g' ${file_to_edit}",
+  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 4096\"/g' /etc/default/nginx",
   path    => ['/bin','/usr/bin']
 } ->
 
